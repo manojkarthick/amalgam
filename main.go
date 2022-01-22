12fc1e0614492e03c5b695b889b016fbc1405328
Manoj Karthick <manojkarthick@users.noreply.github.com>
2022-01-22 01:20:36 -0800

    create mvp with download, combine and upload features

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	// setup logging configuration
+	setupLogging()
+
+	cfg := &Configuration{}
+
+	app := &cli.App{
+		Name:  "amalgam",
+		Usage: "Create macOS Universal binaries from Github releases",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "owner",
+				Required:    true,
+				Usage:       "Github repo owner username ",
+				Destination: &cfg.Owner,
+			},
+			&cli.StringFlag{
+				Name:        "repo",
+				Required:    true,
+				Usage:       "Github repository name",
+				Destination: &cfg.Repository,
+			},
+			&cli.StringFlag{
+				Name:        "tag",
+				Required:    false,
+				Usage:       "Github repository name",
+				Value:       "latest",
+				Destination: &cfg.Tag,
+			},
+			&cli.StringFlag{
+				Name:        "amd64",
+				Required:    true,
+				Usage:       "Substring for the amd64 binary",
+				Destination: &cfg.Amd64Substring,
+			},
+			&cli.StringFlag{
+				Name:        "arm64",
+				Required:    true,
+				Usage:       "Substring for the arm64 binary",
+				Destination: &cfg.Arm64Substring,
+			},
+			&cli.BoolFlag{
+				Name:        "compressed",
+				Required:    false,
+				Usage:       "Do the releases use compressed archives",
+				Destination: &cfg.Compressed,
+			},
+			&cli.StringFlag{
+				Name:        "binary-path",
+				Required:    false,
+				Usage:       "Path to the binary inside the archive",
+				Destination: &cfg.BinaryPath,
+			},
+			&cli.BoolFlag{
+				Name:        "overwrite",
+				Required:    false,
+				Usage:       "Delete pre-existing universal asset?",
+				Destination: &cfg.Overwrite,
+			},
+		},
+		Action: func(context *cli.Context) error {
+			log.Debug("Num flags: ", context.NumFlags())
+			if context.NumFlags() < 4 {
+				cli.ShowAppHelpAndExit(context, 1)
+			}
+
+			token := os.Getenv("GITHUB_TOKEN")
+			if token == "" {
+				log.Fatal("GITHUB_TOKEN environment variable is empty.")
+				os.Exit(2)
+			}
+
+			cfg.GithubToken = token
+
+			err := CreateUniveralBinary(cfg)
+			if err != nil {
+				log.Fatal("Exiting. Encountered exception: ", err)
+				os.Exit(1)
+			}
+
+			log.Info("Complete.")
+			return nil
+		},
+	}
+
+	// run command line application and exit on error
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalf("Encountered error: %s", err.Error())
+		log.Fatal("Exiting...")
+		os.Exit(1)
+	}
+
+}
---
+++ utils.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	UNIVERSAL_IDENITIFER = "universal_test"
+	AMD64_IDENTIFIER     = "amd64"
+	X64_IDENTIFER        = "x64"
+	X86_64_IDENTIFER     = "x86_64"
+)
+
+// setup global logging configuration and make logging level
+// configurable using LOG_LEVEL environment variable
+func setupLogging() {
+	lvl, ok := os.LookupEnv("LOG_LEVEL")
+	if !ok {
+		lvl = "debug"
+	}
+	ll, err := log.ParseLevel(lvl)
+	if err != nil {
+		ll = log.DebugLevel
+	}
+
+	customFormatter := new(log.TextFormatter)
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	customFormatter.FullTimestamp = true
+	log.SetFormatter(customFormatter)
+
+	log.SetOutput(os.Stdout)
+	log.SetLevel(ll)
+}
+
+// download file located at the given URL to filepath
+func downloadFile(filepath string, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+// generate universal binary asset name for github based on arch
+func generateUniversalAssetName(amd64AssetName string) string {
+	var architecture string
+
+	// if the asset name contains amd64, x64 or x86_64, convert
+	// that architecture identifier to the universal identifier.
+	// assumes that the architecture is x86_64 if nothing is provided.
+	if strings.Contains(amd64AssetName, AMD64_IDENTIFIER) {
+		architecture = AMD64_IDENTIFIER
+	} else if strings.Contains(amd64AssetName, X64_IDENTIFER) {
+		architecture = X64_IDENTIFER
+	} else {
+		architecture = X86_64_IDENTIFER
+	}
+
+	return strings.ReplaceAll(amd64AssetName, architecture, UNIVERSAL_IDENITIFER)
+}
